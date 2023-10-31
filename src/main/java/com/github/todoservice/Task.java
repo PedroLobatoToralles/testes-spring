642c88e6d5a4becc3796fb90b0761d8d17dbf60c
Pedro Lobato Toralles <134618293+PedroLobatoToralles@users.noreply.github.com>
2023-10-31 11:54:00 -0300

    Add files via upload

---
+++ src/main/java/com/github/todoservice/Task.java
@@ -0,0 +1,9 @@
+package com.github.todoservice;
+
+public class Task {
+    public int id = 0;
+    public String descricao = "nao sei";
+    public String vencimento = "0/0/2023";
+
+    public boolean concluida = false;
+}
